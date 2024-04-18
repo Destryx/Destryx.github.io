039256be12bd89b4d08f80af403bc005bd9a9709
Destryx <95033492+Destryx@users.noreply.github.com>
2024-04-19 01:14:39 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ var l_displayMapGamemode = true;
 
 // Custom server name configuration
 // Leave it blank for automatic server name
-var l_serverName = "Rescapé de Reach";
+var l_serverName = "";
 
 // Use a video for the background?
 // Otherwise it will use images
@@ -72,8 +72,7 @@ var l_bgImageMapBased = false;
 // (Place the files in the backgrounds/images folder)
 // (You can also use external URL)
 var l_bgImages = [
-	"reach.jpg",
-	"odst.jpg",
+	"beta.jpg",
 ];
 
 // (Image-background only)
