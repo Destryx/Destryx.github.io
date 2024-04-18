853bae51bfe10dfba008bcdc4068a11eca503576
Destryx <95033492+Destryx@users.noreply.github.com>
2024-04-19 01:30:46 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -111,6 +111,7 @@ var l_musicDisplay = false;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
+	{youtube: "", name: ""},
 ];
 
 // Random music order?
